94bf59a518e74e648a3ba833b4ed009a37aa404f
Jackson Clary <141957445+t0ll1v3r@users.noreply.github.com>
2025-01-22 13:51:53 -0500

    Update mail_client.py

--- MailServerLab/mail_client.py
+++ MailServerLab/mail_client.py
@@ -1,4 +1,9 @@
-# In this lab, you will develop a web server that handles one HTTP request at a time. Your web server should accept and parse the HTTP request, get the requested file from the server’s file system, create an HTTP response message consisting of the requested file preceded by header lines, and then send the response directly to the client. If the requested file is not present in the server, the server should send an HTTP “404 Not Found” message back to the client.
+# This lab entailed developing a web server which handles one HTTP request at a time. 
+# The web server should accept and parse the HTTP request, get the requested file from the
+# server’s file system, create an HTTP response message consisting of the requested file
+# preceded by header lines, and then send the response directly to the client. If the
+# requested file is not present in the server, the server should send an HTTP “404 Not
+# Found” message back to the client. 
 
 from socket import *
 import sys
